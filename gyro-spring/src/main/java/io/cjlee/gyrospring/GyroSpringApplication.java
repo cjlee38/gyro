da0d7a2b4ecbdb3dd6b2fa3c86f81d93ff7f86b7
cjlee38 <gptpem38@gmail.com>
2024-03-19 12:41:44 +0900

    chore: add spring module

---
+++ gyro-spring/src/main/java/io/cjlee/gyrospring/GyroSpringApplication.java
@@ -0,0 +1,13 @@
+package io.cjlee.gyrospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyroSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GyroSpringApplication.class, args);
+	}
+
+}
